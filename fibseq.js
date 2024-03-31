80e006a32014ee12adf513dd730bc07b506d7869
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-03-30 21:26:38 -0700

    Finish iterative fibonacci func

--- fibseq.js
+++ fibseq.js
@@ -1,2 +1,29 @@
-console.log("test");
+/*
+
+//Iterative fibonacci sequence up to 8:
+
+function fibs(num) {
+    let arr = [];
+
+    for (let i = 0; i < num; i++) { 
+        if (i > 1) {
+            let oneStep = arr[i - 1] 
+            let twoStep = arr[i - 2]
+            let next = oneStep + twoStep
+
+            arr.push(next);
+        } else {
+            arr.push(i);
+        }
+    }
+
+    return arr;
+}
+
+let fibs8 = fibs(8);
+
+console.log(fibs8);
+    
+*/
+
 
