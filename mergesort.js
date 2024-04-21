0e950e1036c8d8bee6b6ec2b4464b688fcb37503
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-04-21 14:52:02 -0700

    Finish mergeSort

--- mergesort.js
+++ mergesort.js
@@ -24,48 +24,29 @@ merge sort (arr) {
 
 */
 
-function mergeSort(arr, sorted = []) {
-    let start = 0
-    let end = (arr.length - 1) + 1;
-    let middle = (arr.length / 2);
-    
-    let leftSegment = arr.slice(start, middle);
-    let rightSegment = arr.slice(middle, end);
+function mergeSort(arr) {
+    const half = arr.length /  2;
 
-    //merge function steps
-
-    //loop through any segment 
-    //use i as the index pointer for both segments
-    //if one segment is longer (no more values to compare),
-    //push remaining values to "sorted array"
-
-
-    if (arr.length === 1) {
-        return;
-    } else {
-        mergeSort(leftSegment);
-        mergeSort(rightSegment);
-        for (let i = 0; i < leftSegment.length; i++) {
-            //merge function
-        }
-        
+    if (arr.length < 2) {
+        return arr;
     }
 
-    if (arr.length === 1) {
-        if (leftSegment > rightSegment) {
-            return leftSegment;
-        } else {
-            
-        }
-        console.log(arr);
-    }
-
-
-} 
-
-function largerSegment(segment) {
-
+    const left = arr.splice(0, half);
+    return merge(mergeSort(left), mergeSort(arr));
 }
+
+function merge(left, right, arr = []) {
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            arr.push(left.shift());
+        } else {
+            arr.push(right.shift());
+        }
+    }
+
+    return [ ...arr, ...left, ...right ];
+}
+
 
 
 let exampleInput = mergeSort([3, 2, 1, 13, 8, 5, 0, 1]);
